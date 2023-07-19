b7b7fca8bb4af3bd96ac37e09ff81d57452a1356
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-07-19 21:56:04 +0630

    added foods

---
+++ Features/Food/FoodController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryApp.Features.Food
+{
+    public class FoodController : Controller
+    {
+        public IActionResult PopularFood()
+        {
+            return View();
+        }
+
+    }
+}
